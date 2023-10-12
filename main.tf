750c34435bce1487b9d7ca998e8df1918bcd39d0
p-singhR <147203908+p-singhR@users.noreply.github.com>
2023-10-13 02:10:39 +0530

    Delete main.tf

--- main.tf
+++
@@ -1,7 +0,0 @@
-resource "google_storage_bucket" "terra-bucket" {
-    name= "prerna-terra-bucket" 
-    project=    "prj-1909-ay51983332sh-svc"
-    location= "europe-north1"
-    storage_class = "standard"
-    uniform_bucket_level_access = "true"
-}
