e5774755d558b23395b0b5e7c939ecca4a8454cf
p-singhR <147203908+p-singhR@users.noreply.github.com>
2023-10-13 00:25:56 +0530

    Update main.tf
    testing

--- main.tf
+++ main.tf
@@ -1,21 +1,7 @@
-resource "google_compute_instance" "prerna-terravm-vpcauto" {
-  machine_type = "e2-medium"
-  name         = "prerna-terravm-vpcauto"
-  zone = "europe-north1-a"
-  tags = ["https-server", "http-server"]
-  boot_disk {
-    initialize_params {
-      image = "projects/kube-project-380713/global/images/centos-7-hardened-custom-os-image"
-      size  = 20
-      type  = "pd-balanced"
-    }
-    mode = "READ_WRITE"
-  }
-  
-  network_interface {
-    network = "projects/prj-1909-ay51983332sh-svc/global/networks/prerna-vpc"
-  }
-
-  
+resource "google_storage_bucket" "terra-bucket" {
+    name= "prerna-terra-bucket" 
+    project=    "prj-1909-ay51983332sh-svc"
+    location= "europe-north1"
+    storage_class = "standard"
+    uniform_bucket_level_access = "true"
 }
-
