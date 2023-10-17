3cbb4fd9fc16b5c0695c02ea95992d87cc9c7981
p-singhR <147203908+p-singhR@users.noreply.github.com>
2023-10-17 19:39:12 +0530

    Update main.tf

--- template/main.tf
+++ template/main.tf
@@ -1,13 +1,13 @@
 module "filestore_template" {
   source = "git::git@github.com:p-singhR/ojt.git/module"
-  project = "prj-1909-ay51983332sh-svc"
-  location = "europe-north1-a"
-  name = "prerna-fs"
-  tier = "BASIC_HDD"
-  capacity = "1024"
-  filestore = "filestore"
-  network = "projects/prj-13052023-h-landing-zone-01/global/networks/vpc-shr-nprd-int-01"
-  modes = ["ADDRESS_MODE_UNSPECIFIED"]
-  connect_mode = "PRIVATE_SERVICE_ACCESS"
+  project = var.project
+  location = var.location
+  name = var.name
+  tier = var.tier
+  capacity = var.capacity
+  filestore = var.filestore
+  network = var.network
+  modes = var.modes
+  connect_mode = var.connect_mode
 
 }
